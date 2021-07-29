45c98b8370f1821c194b68b58ec365503444f17d
vitalik57 <77902972+vitalik57@users.noreply.github.com>
2021-07-29 11:49:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./assets/fonts/fonts.css";
-import Modal from "./components/DiaryAddProductForm/AddProductModal";
 
 ReactDOM.render(
   <React.StrictMode>
